b69b7b58f9aae5682c02e045629b2737eb6ebc83
Henry Bbosa <bbosa.henry1@gmail.com>
2018-12-22 10:29:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const witchcraft = (function(){
+    const  balancedStrings =()=>{
+        return true;
+    }
+    return {
+        balancedStrings
+    }
+})()
+
+module.exports = witchcraft;
