b432184963920d8307e3bde02f2bf44c8c3f204a
Henry Bbosa <bbosa.henry1@gmail.com>
2018-12-22 14:33:50 -0500

    isBracketBalanced

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 
 const witchcraft = (function(){
-    const  balancedStrings =()=>{
-        return true;
-    }
+   
+   const isBracketBalanced=(str)=>{
+        var i, ch;
+      
+        var bracketsMap = new Map();
+        bracketsMap.set(']', '[');
+        bracketsMap.set('}', '{');
+        bracketsMap.set(')', '(');
+      
+        // Use the spread operator to transform a map into a 2D key-value Array.
+        var closingBrackets = [...bracketsMap.keys()];
+        var openingBrackets = [...bracketsMap.values()];
+      
+        var temp = [];
+        var len = str.length;
+      
+        for (i = 0; i < len; i++) {
+          ch = str[i];
+      
+          if (openingBrackets.indexOf(ch) > -1) {
+            temp.push(ch);
+          } else if (closingBrackets.indexOf(ch) > -1) {
+      
+            var expectedBracket = bracketsMap.get(ch);
+            if (temp.length === 0 || (temp.pop() !== expectedBracket)) {
+              return false;
+            }
+      
+          } else {
+            // Ignore the characters which do not match valid Brackets symbol
+            continue;
+          }
+        }
+      
+        return (temp.length === 0);
+      }
     return {
-        balancedStrings
+        isBracketBalanced
     }
 })()
 
